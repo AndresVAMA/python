6015057f1d19c2d99dda1b2cc9949022074bb333
Andrés Vázquez Macías <108751062+AndresVAMA@users.noreply.github.com>
2022-07-05 16:33:32 -0500

    Add files via upload

---
+++ DSyML_Grid_Search_con_ElasticNet.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("../DATA/Advertising.csv")
+df.head()
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+
+##CREATE X and y##
+X = df.drop('sales',axis=1)
+y = df['sales']
+
+##TRAIN TEST SPLIT##
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+##SCALE DATA##
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+##Modelo##
+base_elastic_net_model = ElasticNet()
+param_grid = {'alpha':[0.1,1,5,10,50,100],'l1_ratio':[.1, .5, .7, .9, .95, .99, 1]}
+grid_model = GridSearchCV(estimator=base_elastic_net_model,param_grid=param_grid,scoring='neg_mean_squared_error',cv=5,verbose=2)
+grid_model.fit(X_train,y_train)
+grid_model.best_estimator_
+grid_model.best_params_
+pd.DataFrame(grid_model.cv_results_)
+y_pred = grid_model.predict(X_test)
+mean_squared_error(y_test,y_pred)
